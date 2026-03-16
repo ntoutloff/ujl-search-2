8918c57685251a1f4204531c67778d62b6d53f39
ntoutloff <ntoutloff@gmail.com>
2026-03-16 18:07:14 -0500

    Display publishers if there are any

--- static/js/override.js
+++ static/js/override.js
@@ -37,45 +37,46 @@ clearJournalNamesBtn.addEventListener('click', function () {
     const radioBtns = document.querySelectorAll('input[type="radio"][name="journal_name_override"]');
     radioBtns.forEach(function (radioBtn) {
         radioBtn.checked = false;
-    })
-})
+    });
+});
 
 
 // Publisher name
 if (journalData.publisher_names.length >= 1) {
 
-console.log(journalData.publisher_names);
-for (const value of journalData.publisher_names) {
-    const formCheck = document.createElement('div');
-    formCheck.className = 'form-check';
-    publisherNamesRadioList.appendChild(formCheck);
+    document.getElementById('publisher-div').style = 'display: block;';
 
-    const formInput = document.createElement('input');
-    Object.assign(formInput, {
-        className: 'form-check-input',
-        type: 'radio',
-        name: 'publisher_name_override',
-        id: value,
-        value: value
+    console.log(journalData.publisher_names);
+    for (const value of journalData.publisher_names) {
+        const formCheck = document.createElement('div');
+        formCheck.className = 'form-check';
+        publisherNamesRadioList.appendChild(formCheck);
+
+        const formInput = document.createElement('input');
+        Object.assign(formInput, {
+            className: 'form-check-input',
+            type: 'radio',
+            name: 'publisher_name_override',
+            id: value,
+            value: value
+        })
+        formCheck.appendChild(formInput);
+
+        const formLabel = document.createElement('label');
+        Object.assign(formLabel, {
+            className: 'form-check-label',
+            htmlFor: value,
+            textContent: value
+        })
+        formCheck.appendChild(formLabel);
+    }
+
+    // Publisher names clear button
+    const clearPublisherNamesBtn = document.getElementById('uncheck-publisher-names');
+    clearPublisherNamesBtn.addEventListener('click', function () {
+        const radioBtns = document.querySelectorAll('input[type="radio"][name="publisher_name_override"]');
+        radioBtns.forEach(function (radioBtn) {
+            radioBtn.checked = false;
+        })
     })
-    formCheck.appendChild(formInput);
-
-    const formLabel = document.createElement('label');
-    Object.assign(formLabel, {
-        className: 'form-check-label',
-        htmlFor: value,
-        textContent: value
-    })
-    formCheck.appendChild(formLabel);
-}
-
-// Publisher names clear button
-const clearPublisherNamesBtn = document.getElementById('uncheck-publisher-names');
-clearPublisherNamesBtn.addEventListener('click', function () {
-    const radioBtns = document.querySelectorAll('input[type="radio"][name="publisher_name_override"]');
-    radioBtns.forEach(function (radioBtn) {
-        radioBtn.checked = false;
-    })
-})
-
 }
