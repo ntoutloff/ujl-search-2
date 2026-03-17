15a205c3e9f1bdada05be01902c9f46b8f3e4b18
Nathan <ntoutloff@gmail.com>
2026-03-17 17:04:26 -0500

    scratch

--- scratch.py
+++ scratch.py
@@ -2,58 +2,83 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 import json
+from typing import Any
 
 
 load_dotenv()
 
 URI = os.getenv('MONGO_URI')
+issn_chain = ['0360-1315', '1873-782X']
 
 with MongoClient(URI) as client:
-    or_collection = client['UJL']['overrides']
-    ujl_collection = client['UJL']['universalJournalList']
-    override_doc = or_collection.find_one({'value': 'Fish Pathol'})
+    issns_collection = client['UJL']['issns']
 
+    issn_docs: list[dict[str, list[str] | dict[str, Any]]] = issns_collection.find({'issn': {'$in': issn_chain}}).to_list(100)
+
+    upload_doc: dict[str, list[str] | dict[str, Any]] = {
+        "issns": issn_chain,
+        "journal_names": list({name for doc in issn_docs for name in doc.get("journal_names", [])}),
+        "publisher_names": list({name for doc in issn_docs for name in doc.get("publisher_names", [])}),
+        "locations": {},
+    }
+    for doc in issn_docs:
+        for location, records in doc.get("locations", {}).items():
+            upload_doc["locations"].setdefault(location, [])
+            upload_doc["locations"][location].extend(r["data"] for r in records if r["data"] not in upload_doc["locations"][location])
+
+    # generate a dict of name and locations
+    sort_dict = {}
+   
     
-    ujl_docs = ujl_collection.find(
-        {
-            '$and': [
-                {'journal_names': override_doc['value']},
-                {'journal_names': {'$in': override_doc['overrides']}}
-            ]
-        }
-    ).to_list(10)
-    
-    if len(ujl_docs) == 1:
-        doc_id = ujl_docs[0]['_id']
-        print(doc_id)
-        res = ujl_collection.update_one(
-            {"_id": doc_id},
-            [
-                {
-                    "$set": {
-                        "journal_names": {
-                            "$concatArrays": [
-                                [override_doc['value']],
-                                {
-                                    "$filter": {
-                                        "input": "$journal_names",
-                                        "cond": {"$ne": ["$$this", override_doc['value']]}
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            ]
-        )
-        print(res)
+    for doc in issn_docs:
+        for location_name in doc['locations']:
+            for record in doc['locations'][location_name]:
+                for name in record['journal_names']:
+                    my_name = ''
+                    if location_name == 'pubmed' and name != record['data']['JournalTitle']:
+                        my_name = 'alts'
 
+                    elif location_name == 'openAlex' and name != record['data']['display_name']:
+                        my_name = 'alts'
+                    
+                    else:
+                        my_name = location_name
 
+                    if name not in sort_dict:
+                        
+                        sort_dict[name] = {my_name}
+                    else:
+                        sort_dict[name].add(my_name)
 
-    elif len(ujl_docs) < 1:
-        print('no results')
+    sort_dict = {k: list(v) for k, v in sort_dict.items()} 
+    print(json.dumps(sort_dict, indent=4))
 
-    else:
-        print(f'multiple ({len(ujl_docs)}) results were found')
+    source_list = [
+        'zoho',
+        'predatoryReports',
+        'ebsco',
+        'abdc',
+        'jufo',
+        'pubmed',
+        'doaj',
+        'scopus',
+        'scite',
+        'scimago',
+        'openAlex',
+        'crossref',
+        'alts'
+    ]
+
+    my_list = []
+
+    print(upload_doc['journal_names'])
+    for source in source_list:
+        for journal_name, locations in sort_dict.items():
+            if source in locations:
+                if journal_name not in my_list:
+                    my_list.append(journal_name)
+                
+    print(my_list)
+
     
 
